958a6239e53de4ce98da38a30c9135f8426db56f
ahugla <ahugla@vmware.com>
2021-04-19 22:18:33 +0200

    Update install.sh

--- k8s/install.sh
+++ k8s/install.sh
@@ -16,11 +16,13 @@ git clone https://github.com/ahugla/Docaposte.git
 
 
 # CREATE ENV
-kubectl create -f /tmp/Docaposte/lamp-k8s-namespace.yaml
+kubectl create -f /tmp/Docaposte/k8s/lamp-k8s-namespace.yaml
 sleep 3
-kubectl apply -f php-content-configmap.yaml
-kubectl apply -f apache-configmap.yaml
-kubectl apply -f php-fpm-deployment.yaml
-kubectl apply -f apache-deployment.yaml
-kubectl apply -f php-fpm-service.yaml
-kubectl apply -f apache-service.yaml
+kubectl apply -f /tmp/Docaposte/k8s
+
+#kubectl apply -f php-content-configmap.yaml
+#kubectl apply -f apache-configmap.yaml
+#kubectl apply -f php-fpm-deployment.yaml
+#kubectl apply -f apache-deployment.yaml
+#kubectl apply -f php-fpm-service.yaml
+#kubectl apply -f apache-service.yaml
