fa47c1993289cb0efd91181dfb6e5b36e1d24f96
ahugla <ahugla@vmware.com>
2021-04-19 22:03:25 +0200

    update

---
+++ k8s/install.sh
@@ -0,0 +1,23 @@
+
+
+cd /tmp
+
+
+# DELETE ENV
+kubectl delete services lamp-php-fpm  lamp-apache  -n lamp-k8s
+kubectl delete deployment lamp-php-fpm lamp-apache -n lamp-k8s
+kubectl delete configmap php-content apache-conf   -n lamp-k8s
+kubectl delete namespace lamp-k8s
+
+
+# GET NEW SOURCES
+rm -rf /tmp/Docaposte
+git clone https://github.com/ahugla/Docaposte.git
+
+
+
+# CREATE ENV
+kubectl apply -f /tmp/Docaposte/k8s
+
+
+
